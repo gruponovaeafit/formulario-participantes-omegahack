b53014b6a5293bb582e5d6ce0a5e31b36732b98e
Kevin Quiroz <kquirozg@eafit.edu.co>
2024-04-10 20:44:51 -0500

    3 screen created succesfully

--- app/forms/form_sections.py
+++ app/forms/form_sections.py
@@ -30,12 +30,10 @@ class PersonalInformationForm(FlaskForm):
     semestre = IntegerField('Semestre', validators=[DataRequired(), NumberRange(min=1, max=10, message="El semestre debe estar entre 1 y 10.")])
     eps = StringField('EPS', validators=[DataRequired()])
     contacto_emergencia = StringField('Número de Celular', validators=[DataRequired()])
-    parentesco_contacto_emergencia = StringField('Parentesco', validators=[DataRequired()])
     alergias = StringField('Alergias', validators=[DataRequired()])
     enfermedades = StringField('Enfermedades', validators=[DataRequired()])
     numero_contacto_emergencia = StringField('Número de contacto de emergencia', validators=[DataRequired()])
-    seguridad_asistencia = BooleanField('He leído y acepto los términos y condiciones', validators=[DataRequired(message="Debe aceptar la política de tratamiento de datos para proceder.")])
-    submit = SubmitField('Enviar')
+    parentesco_contacto_emergencia = StringField('Parentesco', validators=[DataRequired()]) 
 
     def validate_semestre(self, field):
         if field.data < 1 or field.data > 10:
